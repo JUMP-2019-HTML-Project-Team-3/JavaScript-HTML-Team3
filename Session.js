127e077f46364cbd7cf90649f95fde734bc6a466
Gregory <itsgregreed@gmail.com>
2019-09-11 14:08:45 -0400

    changes

--- Session.js
+++ Session.js
@@ -18,6 +18,24 @@ function User(userName, password){
     this.password = password;
 }
 
+function setLinks()
+{
+	var parameters = location.search.split("?");
+	document.getElementById("displayName").innerText = parameters[1];
+	
+	var join = "../Index/Index.html?" + parameters[1];
+	document.getElementById("indexPage").href = join;
+
+	var join = "#?" + parameters[1];
+	document.getElementById("gregPage").href = join;
+
+	var join = "../Omran/Omran_webpage.html?" + parameters[1];
+	document.getElementById("omranPage").href = join;
+
+	var join = "../Rebecca/RebeccaPage.html?" + parameters[1];
+	document.getElementById("rebeccaPage").href = join;
+}
+
 function checkUser(){
     var check = document.getElementById("sb1");
     var tryUser = document.getElementById("uID").value;
