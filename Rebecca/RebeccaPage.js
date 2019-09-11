4c2dc3125f440dbec3f00dd3c961665e51816de7
[Rebecca Hull] <[RLHull@my.harrisburgu.edu]>
2019-09-11 09:07:27 -0400

    Cookie test

--- Rebecca/RebeccaPage.js
+++ Rebecca/RebeccaPage.js
@@ -12,10 +12,40 @@ function domloaded(){
     ctx.strokeText("Salki's Noms",10,50);
 }
 
-function checkCookie() {
-	
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var user=getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  }
+}
 function moreInfo() {
   var txt;
   var more = prompt("Anything else you'd like to tell us? Here's your chance!");
